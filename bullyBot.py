9875affc00630a1f91b5bbea5ed525abc4dbc26c
theArgo15 <70275138+theArgo15@users.noreply.github.com>
2020-11-13 18:04:20 -0600

    Add list for multiple responses, shebang, comments

--- bullyBot.py
+++ bullyBot.py
@@ -1,5 +1,7 @@
-#bullyBot.py
+#!/usr/bin/env python3
+# bullyBot.py
 import os
+import random
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -9,6 +11,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
+
 
 @bot.event
 async def on_ready():
@@ -21,29 +24,48 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})'
     )
 
+
 @bot.command(name='bully', help='Creates a new channel for bullying a new member')
 async def on_message(ctx, *args):
-    
-    #if args is len is 0 you want to deliever help dialog, otherwise you want to grab the first arg as victim
+
+    # if args is len is 0 you want to deliever help dialog, otherwise you want to grab the first arg as victim
     print(args)
     if len(args) == 0:
         await ctx.send('Type the name of the person you want to bully after !bully')
     else:
         victim = args[0]
 
-        victim=victim.lower().strip()
-        channel_name=f'making-fun-of-{victim}'
+        victim = victim.lower().strip()
+        channel_name = f'making-fun-of-{victim}'
         guild = ctx.guild
+        #look to see if there is already a channel for this victim
         existing_channel = discord.utils.get(guild.channels, name=channel_name)
-        bullying_category = discord.utils.get(guild.categories, name='bullying')
+        bullying_category = discord.utils.get(
+            guild.categories, name='bullying')
 
         if not existing_channel:
-            response = f'Good choice, {victim} is due for a bullying'
-            #print(f'Creating a new channel for the purpose of bullying {victim}.')
+            #create a new channel
+            positive_responses = [
+                f"Oh yeah, it's time to bully {victim}",
+                f"Good choice, {victim} is due for a bullying",
+                f"You're right, {victim} was getting too uppity",
+                f"{victim} needs to be taken down a notch",
+                f"{victim}!, I hate that guy"
+                f"Okay, I'm going to open a channel to bully {victim}. Don't be too harsh, {victim} is a delicate flower"
+            ]
+            response = random.choices(positive_responses)[0]
+            print(f'Creating a new channel for the purpose of bullying {victim}.')
+
             await guild.create_text_channel(channel_name, category=bullying_category)
 
         else:
-            response = f'Hey now, we already have a channel for bullying {victim}'
+            #make a sassy remark about how there is already a channel for this victim
+            negative_responses = [
+                f"If you want to bully {victim}, there is already a channel for that",
+                f"Hey now, we already have a channel for bullying {victim}",
+                f"I would appreciate it if you looked to see if there was a channel for bullying {victim} before you ask me to make one"
+            ]
+            response = random.choices(negative_responses)[0]
 
         await ctx.send(response)
 
