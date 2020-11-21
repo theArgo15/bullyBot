ff3de679bef6847046b2b03fa53282c70d346c90
Antonio Woods <antonio95woods@gmail.com>
2020-11-20 21:03:31 -0500

    Add more insults

--- bullyBot.py
+++ bullyBot.py
@@ -12,6 +12,34 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
 
+pick = lambda list: random.choices(list)[0]
+
+adjectives = [
+        "blathering",
+        "filthy",
+        "massive",
+        "unholy",
+        "buttnaked",
+        "cattywampus",
+        "bloviating",
+        "disgusting",
+        "contemptable",
+        "vile",
+        "smelly",
+        "buttlicking",
+        "shitfilled",
+]
+
+nouns = [
+        "piece of shit",
+        "slice of cheese",
+        "walrus",
+        "chicken fucker",
+        "turkey leg of a person",
+        "bumfuzzle",
+        "lollygag",
+        "diddle daddler",
+]
 
 @bot.event
 async def on_ready():
@@ -44,8 +72,55 @@ async def on_message(ctx, *args):
             f"If {target} was a spice, they'd be flour",
             f"{target} thought Atlas Shrugged was an interesting story",
             f"{target} fully believed the earth was flat well into their teens"
+            f"{target} chews with their mouth open",
+            f"{target} thinks they shit don't stank, but it do",
+            f"{target} wipes their ass from back to front",
+            f"{target}'s favorite comedian is Jeff Dunham",
+            "Walruses can't feel love",
+            f"{target} smells mildly of cheese",
+            f"{target} never uses their goddamn turn signal",
+            f"{target} is the Ringo Star of this friend group",
+            f"{target}'s pee is dark yellow cuz they never drink water",
+            f"{target} once tried to fart in someone's general direction as a prank, but they full on shat themselves and played it off like nothing happened.",
+            f"{target}'s ringtone is Womanizer by Britney Spears",
+            f"{target} prefers their french fries reheated",
+            f"{target} wonders when Tupac is going to make his comeback",
+            f"{target} rolls down their windows and blasts their music because they think they have cool taste and want validation from strangers",
+            f"To be honest, I just fucking hate {target}",
+            f"{target} thinks Aunt Jemima is better than Mrs Butterworth's",
+            f"{target} is probably a baby rapist",
+            f"{target} falls asleep to scat porn because it calms their nerves",
+            f"{target} has never seen their butthole in person",
+            f"{target} probably hasn't gone outside today",
+            f"{target} is a lying dog-faced pony soldier",
+            f"{target} types with just two fingers",
+            f"{target} sucks ass",
+            f"{target} pours their milk before their cereal",
+            f"{target}'s favorite cereal is chex",
+            f"{target} comes from the wrong nut",
+            f"{target} asks fat women when they're due",
+            f"{target} pretends to have forgotten their wallet, but really they just don't want to pay.",
+            f"{target} talks loudly on the phone in the metro",
+            f"{target} farts in elevators just before they leave",
+            f"{target} likes to sing along with songs, but they are completely tone deaf",
+            f"{target} talks about you behind your back",
+            f"{target} lets the water run, but doesn't actually wash their hands",
+            f"{target} spits when they talk",
+            f"{target} is secretly attracted to the mom from Rolie Polie Olie",
+            f"{target} sucks ass at foosball. Challenge me, pussy.  You don't stand a fucking chance.",
+            f"{target} always showers right before they shit",
+            f"{target} has small genitles. "*32,
+            f"Fuck you. "*100,
+            f"Fuck you {target}.",
+            f"{target} is the most {pick(adjectives)} {pick(adjectives)} {pick(nouns)} I have ever known",
+            f"{target} is a {pick(adjectives)} {pick(adjectives)} {pick(adjectives)} {pick(adjectives)} {pick(adjectives)} {pick(nouns)}",
+            f"{target} is analogous to a {pick(nouns)}",
+            f"The best way I can describe {target} is {pick(adjectives)}",
+            f"{target} has a {pick(adjectives)} butthole",
+            f"{target} can lick my {pick(adjectives)} ass",
+            f"{target} looks like a {pick(adjectives)} {pick(nouns)} trying to pass as a {pick(adjectives)} {pick(nouns)}",
         ]
-        response = random.choices(insults)[0]
+        response = pick(insults)
         await ctx.send(response)
 
 
@@ -75,9 +150,9 @@ async def on_message(ctx, *args):
                 f"You're right, {victim} was getting too uppity",
                 f"{victim} needs to be taken down a notch",
                 f"{victim}!, I hate that guy",
-                f"Okay, I'm going to open a channel to bully {victim}. Don't be too harsh, {victim} is a delicate flower"
+                f"Okay, I'm going to open a channel to bully {victim}. Don't be too harsh, {victim} is a delicate flower",
             ]
-            response = random.choices(positive_responses)[0]
+            response = pick(positive_responses)
             print(
                 f'Creating a new channel for the purpose of bullying {victim}.')
 
@@ -88,10 +163,9 @@ async def on_message(ctx, *args):
             negative_responses = [
                 f"If you want to bully {victim}, there is already a channel for that",
                 f"Hey now, we already have a channel for bullying {victim}",
-                f"I would appreciate it if you looked to see if there was a channel for bullying {victim} before you ask me to make one"
+                f"I would appreciate it if you looked to see if there was a channel for bullying {victim} before you ask me to make one",
             ]
-            response = random.choices(negative_responses)[0]
-
+            response = pick(negative_responses)
         await ctx.send(response)
 
 bot.run(TOKEN)
